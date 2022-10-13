8d0529876fc295cb9a9023e7d7f39810c9e13704
Sim Jiajin <backup_sjj@hotmail.com>
2022-10-13 19:10:37 +0800

    add +1/+2 buttons to input calculator field

--- src/components/common/MaterialQuality.js
+++ src/components/common/MaterialQuality.js
@@ -10,12 +10,30 @@ const MaterialQuality = ({ name, quality, value, folder, onChangeHandler }) => {
         src={`../${folder}/item_${name}_${quality}.png`}
         alt=""
       ></img>
-      <div>x</div>
-      <input
-        className="mat__input"
-        value={value}
-        onChange={(e) => onChangeHandler(quality, e.target.value)}
-      ></input>
+      <div className="d-flex flex-column">
+        <div className="d-flex flex-row">
+          <div>x</div>
+          <input
+            className="mat__input"
+            value={value}
+            onChange={(e) => onChangeHandler(quality, e.target.value)}
+          ></input>
+        </div>
+        <div className="d-flex flex-row">
+          <div
+            className="materials_button"
+            onClick={(e) => onChangeHandler(quality, parseInt(value) + 1)}
+          >
+            +1
+          </div>
+          <div
+            className="materials_button"
+            onClick={(e) => onChangeHandler(quality, parseInt(value) + 2)}
+          >
+            +2
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
